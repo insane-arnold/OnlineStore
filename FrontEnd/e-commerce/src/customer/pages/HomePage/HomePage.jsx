0b53ad26ca3385fc48097b8b3a808ffdbd958907
insane_arnold <limbunihal07@gmail.com>
2024-06-21 02:27:22 +0530

    Navigation bar and carousel template

---
+++ FrontEnd/e-commerce/src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainCarousel from '../../components/caroudel/MainCarousel'
+
+const HomePage = () => {
+  return (
+    <div>
+      <MainCarousel />
+      <div>
+        other section
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
