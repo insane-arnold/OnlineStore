7b917e5e07bb4646e28baa75badd48ae49d4a2ae
insane_arnold <limbunihal07@gmail.com>
2024-07-06 17:23:03 +0530

    Home Carousel

--- FrontEnd/e-commerce/src/App.js
+++ FrontEnd/e-commerce/src/App.js
@@ -1,6 +1,6 @@
-import Navigation from './customer/components/Navigation';
 import './App.css';
 import HomePage from './customer/pages/HomePage/HomePage';
+import Navigation from './customer/components/Navigation/Navigation';
 
 function App() {
   return (
